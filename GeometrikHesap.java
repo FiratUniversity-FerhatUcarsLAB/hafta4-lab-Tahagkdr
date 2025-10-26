97313b43f7a7e754029791171bd43c33e4d6d13c
Tahagkdr <250541092@firat.edu.tr>
2025-10-26 17:18:04 +0300

    Update GeometrikHesap.java

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -1,1 +1,39 @@
+import java.util.Scanner;
+
+public class GeometrikHesap {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // Yarıçapbilgisi
+        double yarıçap = input.nextDouble();
+
+        //pi sayısı tanımla
+        double pi = 3.14159f;
+
+        //Matematiksel işlemler
+        double Alan = pi * Math.pow(yarıçap, 2);
+        System.out.printf("\n daire alan: %2f", Alan);
+
+        double Çevre = 2 * pi * yarıçap;
+        System.out.printf("\n daire Çevre: %2f" ,Çevre);
+
+        double çap = 2* yarıçap;
+        System.out.printf("\n çap: %2f", çap);
+
+        double küre_hacimi = 4.0/3.0 * pi *Math.pow(yarıçap, 3);
+        System.out.printf("\n küre_hacimi : %2f", küre_hacimi);
+
+        double küre_yüzey_alanı = 4 * pi * Math.pow(yarıçap,2 );
+        System.out.printf("\n küre_yüzey_alanı: %2f" ,küre_yüzey_alanı );
+
+
+
+
+
+
+
+
+    }
+}
+
+
 
